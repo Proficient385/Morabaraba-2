6b55fb8f3c600bd40a1b18b68d58fed53c386d21
Proficient385 <NProficient384@gmail.com>
2018-03-25 17:00:50 +0200

    Created a player class to be used for each player

---
+++ Morabaraba-2/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Morabaraba_2
+{
+    class Player
+    {
+        public string symbol { get; private set; }
+        public int lives { get; set; }
+        private string stage;
+        public List<Point> playedPos;
+        public List<Point> mill_List;
+        Ellipse Piece1;
+
+        public Player(string symbol, Ellipse Piece1)
+        {
+            this.symbol = symbol;
+            this.Piece1 = Piece1;
+            playedPos = new List<Point>();
+            mill_List = new List<Point>();
+            stage = "Placing";
+            lives = 12;
+        }
+
+        public void makeMove(Point P)
+        {
+
+           
+        }
+
+        public void eliminateOpponent(Player X)
+        {
+           
+        }
+    }
+}
+
