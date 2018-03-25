2729cf1dc42faad8ff551796718ee21202e5d433
Proficient385 <NProficient384@gmail.com>
2018-03-25 18:26:57 +0200

    Modified Player class to place the pieces and update player mode

--- Morabaraba-2/MainWindow.xaml.cs
+++ Morabaraba-2/MainWindow.xaml.cs
@@ -20,17 +20,92 @@ namespace Morabaraba_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Player Player1;
+        Player Player2;
+        List<Point> validPos;
+        string currentPlayer;
         public MainWindow()
         {
             InitializeComponent();
+            currentPlayer = "Red";
+
+            validPos = validPosiitons();
             if (this.WindowState == System.Windows.WindowState.Normal)
             {
                 this.WindowState = System.Windows.WindowState.Maximized;
             }
-           
-        }
-        
 
-       
+        }
+         
+        private List<Point> validPosiitons()
+        {
+            List<Point> result = new List<Point>();
+            // A---
+            result.Add(new Point(430, 113));
+            result.Add(new Point(863, 113));
+            result.Add(new Point(1297, 113));
+            // B--
+            result.Add(new Point(575, 260));
+            result.Add(new Point(863, 260));
+            result.Add(new Point(1151, 260));
+            // C--
+            result.Add(new Point(717, 403));
+            result.Add(new Point(863, 403));
+            result.Add(new Point(1009, 403));
+            // D---
+            result.Add(new Point(430, 548));
+            result.Add(new Point(575, 548));
+            result.Add(new Point(717, 548));
+            result.Add(new Point(1009, 548));
+            result.Add(new Point(1151, 548));
+            result.Add(new Point(1297, 548));
+            // E--
+            result.Add(new Point(717, 694));
+            result.Add(new Point(863, 694));
+            result.Add(new Point(1009, 694));
+            // F--
+            result.Add(new Point(575, 837));
+            result.Add(new Point(863, 837));
+            result.Add(new Point(1151, 837));
+            // G--
+            result.Add(new Point(430, 982));
+            result.Add(new Point(863, 982));
+            result.Add(new Point(1297, 982));
+            return result;
+        }
+
+        private string swapPlayer(string player)
+        {
+            return player == "Red" ? "Yellow" : "Red";
+        }
+        private void Background_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            Point p = Mouse.GetPosition(Background);
+
+            foreach (Point point in validPos)
+            {
+                if (Math.Abs(p.X - point.X) <= 70 && Math.Abs(p.Y - point.Y) <= 70)
+                {
+                    p = point;
+                    break;
+                }
+            }
+            
+
+            t.Text = string.Format("X : {0}\nY : {1}", p.X, p.Y);
+
+            if (currentPlayer == "Red") Player1.makeMove(p);
+            else Player2.makeMove(p);
+
+            currentPlayer = swapPlayer(currentPlayer);
+            // Canvas
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Player1 = new Player("Red", Piece1, Background);
+            Player2 = new Player("Yellow", Piece2, Background);
+        }
     }
 }
--- Morabaraba-2/Player.cs
+++ Morabaraba-2/Player.cs
@@ -16,27 +16,60 @@ namespace Morabaraba_2
         private string stage;
         public List<Point> playedPos;
         public List<Point> mill_List;
-        Ellipse Piece1;
+        private Ellipse[] pieces;
+        private int pIdx;
+        private Canvas brd;
+        private Ellipse piece;
 
-        public Player(string symbol, Ellipse Piece1)
+        public Player(string symbol, Ellipse piece, Canvas brd)
         {
             this.symbol = symbol;
-            this.Piece1 = Piece1;
+            this.brd = brd;
+            this.piece = piece;
+            pieces = createPlayerPieces();
             playedPos = new List<Point>();
             mill_List = new List<Point>();
             stage = "Placing";
             lives = 12;
+            pIdx = 0;
+        }
+
+        private Ellipse[] createPlayerPieces()
+        {
+            Ellipse[] result = new Ellipse[12];
+            for (int i = 0; i < 12;i++)
+            {
+                Ellipse p = new Ellipse();
+                p.Height = piece.ActualHeight;
+                p.Width = piece.ActualWidth;
+                p.Fill = piece.Fill;
+                brd.Children.Add(p);
+                result[i] = p;
+            }
+
+            return result;
         }
 
         public void makeMove(Point P)
         {
-
-           
+            stage = stageUpdate();
+            if (stage == "Placing")
+            {
+                playedPos.Add(P);
+                Canvas.SetLeft(pieces[pIdx], (P.X - (pieces[pIdx].ActualWidth / 2)));
+                Canvas.SetTop(pieces[pIdx], (P.Y - (pieces[pIdx].ActualHeight / 2)));
+                pIdx++;
+            }
         }
 
+        public string stageUpdate()
+        {
+            stage = (pIdx>=12 ? "Moving":"Placing");
+            return stage = (lives == 3 ? "Flying" : stage);
+        }
         public void eliminateOpponent(Player X)
         {
-           
+            X.lives--;
         }
     }
 }
