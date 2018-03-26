59e0cadad05b0058f9f2427966493cee4df2448f
Proficient385 <NProficient384@gmail.com>
2018-03-26 11:38:32 +0200

    GUI update, added GUI_allign_pieces method to nicely allgign each player pieces on the canvas

--- Morabaraba-2/Player.cs
+++ Morabaraba-2/Player.cs
@@ -27,11 +27,75 @@ namespace Morabaraba_2
             this.brd = brd;
             this.piece = piece;
             pieces = createPlayerPieces();
+            GUI_pieces_allign();
             playedPos = new List<Point>();
             mill_List = new List<Point>();
             stage = "Placing";
             lives = 12;
             pIdx = 0;
+        }
+
+        private void allign_Red_Pieces()
+        {
+            Canvas.SetLeft(pieces[0], -70);
+            Canvas.SetTop(pieces[0], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[1], 20);
+            Canvas.SetTop(pieces[1], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[2], 110);
+            Canvas.SetTop(pieces[2], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[3], -70);
+            Canvas.SetTop(pieces[3], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[4], 20);
+            Canvas.SetTop(pieces[4], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[5], 110);
+            Canvas.SetTop(pieces[5], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[6], -70);
+            Canvas.SetTop(pieces[6], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[7], 20);
+            Canvas.SetTop(pieces[7], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[8], 110);
+            Canvas.SetTop(pieces[8], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[9], -70);
+            Canvas.SetTop(pieces[9], brd.ActualHeight / 1.2);
+            Canvas.SetLeft(pieces[10], 20);
+            Canvas.SetTop(pieces[10], brd.ActualHeight / 1.2);
+            Canvas.SetLeft(pieces[11], 110);
+            Canvas.SetTop(pieces[11], brd.ActualHeight / 1.2);
+        }
+
+        private void allign_Yellow_Pieces()
+        {
+            Canvas.SetLeft(pieces[0], brd.ActualWidth/1.13);
+            Canvas.SetTop(pieces[0], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[1], brd.ActualWidth / 1.070);
+            Canvas.SetTop(pieces[1], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[2], brd.ActualWidth / 1.015);
+            Canvas.SetTop(pieces[2], brd.ActualHeight / 1.7);
+            Canvas.SetLeft(pieces[3], brd.ActualWidth / 1.13);
+            Canvas.SetTop(pieces[3], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[4], brd.ActualWidth / 1.070);
+            Canvas.SetTop(pieces[4], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[5], brd.ActualWidth / 1.015);
+            Canvas.SetTop(pieces[5], brd.ActualHeight / 1.5);
+            Canvas.SetLeft(pieces[6], brd.ActualWidth / 1.13);
+            Canvas.SetTop(pieces[6], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[7], brd.ActualWidth / 1.070);
+            Canvas.SetTop(pieces[7], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[8], brd.ActualWidth / 1.015);
+            Canvas.SetTop(pieces[8], brd.ActualHeight / 1.33);
+            Canvas.SetLeft(pieces[9], brd.ActualWidth / 1.13);
+            Canvas.SetTop(pieces[9], brd.ActualHeight / 1.2);
+            Canvas.SetLeft(pieces[10], brd.ActualWidth / 1.070);
+            Canvas.SetTop(pieces[10], brd.ActualHeight / 1.2);
+            Canvas.SetLeft(pieces[11], brd.ActualWidth / 1.015);
+            Canvas.SetTop(pieces[11], brd.ActualHeight / 1.2);
+        }
+
+        private void GUI_pieces_allign()
+        {
+            if(symbol=="Red") allign_Red_Pieces();
+            else allign_Yellow_Pieces();
+
         }
 
         private Ellipse[] createPlayerPieces()
@@ -46,7 +110,7 @@ namespace Morabaraba_2
                 brd.Children.Add(p);
                 result[i] = p;
             }
-
+            
             return result;
         }
 
@@ -62,7 +126,7 @@ namespace Morabaraba_2
             }
         }
 
-        public string stageUpdate()
+        private string stageUpdate()
         {
             stage = (pIdx>=12 ? "Moving":"Placing");
             return stage = (lives == 3 ? "Flying" : stage);
