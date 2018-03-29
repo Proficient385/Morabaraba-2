5f511ec419158b697851a1232df74bbc39bf913d
Proficient385 <NProficient384@gmail.com>
2018-03-29 12:35:15 +0200

    Created a method that validates the move to prompt, by checking whether the move from is valid

--- Morabaraba-2/MainWindow.xaml.cs
+++ Morabaraba-2/MainWindow.xaml.cs
@@ -42,11 +42,23 @@ namespace Morabaraba_2
             moves = new Point[] { new Point(0,0), new Point(0,0)};
         }
 
+        private void second_Move_request(GUI pg, Point[] moves)
+        {
+            if (!game.player_own_Point(moves[0]))
+            {
+                pg.player_err_msg.Content = "Please select your own\npiece to move!";
+                pg.player_err_msg.Visibility = Visibility.Visible;
+                moves[0].X = moves[0].Y = 0;
+                return;
+            }
+            pg.playerTurn.Content = "Click the \nMOVE TO point";
+            pg.player_err_msg.Visibility = Visibility.Hidden;
+           
+        }
         private void Background_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if(game.mill)
             {
-                //MessageBox.Show("The is mill, first choose a cow to eliminate before continuing");
                 game.messageDisplay("The is mill, first choose a \ncow to eliminate before \ncontinuing, use mouse \nleft click to kill");
                 return;
             }
@@ -57,12 +69,12 @@ namespace Morabaraba_2
 
             if (game.Player1.stage == "Moving" && moves[1].X==0 && game.currentPlayer=="Red")
             {
-                game.player1GUI.playerTurn.Content = "Click the \nMOVE TO point";
+                second_Move_request(game.player1GUI, moves);
                 return;
             }
             else if (game.Player2.stage == "Moving" && moves[1].X == 0 && game.currentPlayer == "Yellow")
             {
-                game.player2GUI.playerTurn.Content = "Click the \nMOVE TO point";
+                second_Move_request(game.player2GUI, moves);
                 return;
             }
             
