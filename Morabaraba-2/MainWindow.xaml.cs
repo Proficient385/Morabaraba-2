ef999abf2f9e4d07f4d18813e78a82795f96b1df
Proficient385 <NProficient384@gmail.com>
2018-03-29 12:00:52 +0200

    Modified the mouse click event to check if one or two moves entries are required

--- Morabaraba-2/MainWindow.xaml.cs
+++ Morabaraba-2/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Morabaraba_2
     public partial class MainWindow : Window
     {
         Constraints game;
-        
+        Point[] moves;
         public MainWindow()
         {
             InitializeComponent();
@@ -39,7 +39,7 @@ namespace Morabaraba_2
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             game = new Constraints(Background);
-           
+            moves = new Point[] { new Point(0,0), new Point(0,0)};
         }
 
         private void Background_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -50,10 +50,28 @@ namespace Morabaraba_2
                 game.messageDisplay("The is mill, first choose a \ncow to eliminate before \ncontinuing, use mouse \nleft click to kill");
                 return;
             }
-            Point p = Mouse.GetPosition(Background);
-            game.gamePlay(p);
+            Point p1 = Mouse.GetPosition(Background);
+
+            if (moves[0].X == 0) moves[0] = p1;
+            else moves[1] = p1;
+
+            if (game.Player1.stage == "Moving" && moves[1].X==0 && game.currentPlayer=="Red")
+            {
+                game.player1GUI.playerTurn.Content = "Click the \nMOVE TO point";
+                return;
+            }
+            else if (game.Player2.stage == "Moving" && moves[1].X == 0 && game.currentPlayer == "Yellow")
+            {
+                game.player2GUI.playerTurn.Content = "Click the \nMOVE TO point";
+                return;
+            }
+            
+            game.gamePlay(moves[0],moves[1]);
+            moves[0].X = moves[0].Y = moves[1].X = moves[1].Y = 0;
+            game.player1GUI.GUI_update();
+            game.player2GUI.GUI_update();
         }
-        
+
         private void Background_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             Point p = Mouse.GetPosition(Background);
