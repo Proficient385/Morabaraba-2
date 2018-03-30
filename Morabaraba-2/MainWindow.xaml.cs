4dd505eb87dd51088ebddc1eeaa91596fb13a709
Proficient385 <NProficient384@gmail.com>
2018-03-30 10:54:05 +0200

    Added a keyboard handler as an alternative to play the game, should mouse clicks fail by chance

--- Morabaraba-2/MainWindow.xaml.cs
+++ Morabaraba-2/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Morabaraba_2
     {
         Constraints game;
         Point[] moves;
+        bool game_start;
         bool game_over;
         public MainWindow()
         {
@@ -31,18 +32,99 @@ namespace Morabaraba_2
             {
                 this.WindowState = System.Windows.WindowState.Maximized;
             }
-            
+            game_start = false;
+            KeyDown += Windows_KeyDown;
         }
-         
-       
-       
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        
+        private void gameStart()
         {
-            game = new Constraints(Background);
-            moves = new Point[] { new Point(0,0), new Point(0,0)};
+            game = new Constraints(background);
+            moves = new Point[] { new Point(0, 0), new Point(0, 0) };
             game_over = false;
-            start.IsEnabled = false;
+            game_start = true;
+
+            Start_game.Visibility = Visibility.Hidden;
+            Quit_game.Visibility = Visibility.Hidden;
+            Instructions.Visibility = Visibility.Hidden;
+            
+            Morabaraba.Background = Brushes.Navy;
+            background.Opacity = 100;
+        }
+
+        private void gameOver(GUI pgWinner, GUI pgLoser)
+        {
+            pgWinner.playerTurn.Visibility = Visibility.Visible;
+            pgLoser.playerTurn.Visibility = Visibility.Visible;
+            pgWinner.player_err_msg.Visibility = Visibility.Visible;
+            pgLoser.player_err_msg.Visibility = Visibility.Visible;
+
+            pgWinner.playerTurn.Content = "WINNER";
+            pgWinner.player_err_msg.Content = "CONGRADULATION\n  you have won the game";
+            pgLoser.playerTurn.Content = "LOSER";
+            pgLoser.player_err_msg.Content = "You have lost the game\n  try harder next time!";
+
+            game_over = true;
+        }
+
+        private void quitGame()
+        {
+            if (MessageBox.Show("Are you sure you want to quit?", "Quit game", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void InstrucuntionManual()
+        {
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"../../Instructions.pdf";
+                Uri pdf = new Uri(path, UriKind.RelativeOrAbsolute);
+                process.StartInfo.FileName = pdf.LocalPath;
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Could not open the file.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Start_game_Click(object sender, RoutedEventArgs e)
+        {
+            gameStart();
+        }
+
+        private void Quit_game_Click(object sender, RoutedEventArgs e)
+        {
+            quitGame();
+        }
+
+        private void Instructions_Click(object sender, RoutedEventArgs e)
+        {
+            InstrucuntionManual();
+        }
+
+        private void Windows_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    if (!game_start) gameStart();
+                    else MessageBox.Show("Game in progress, use only the mouse to play");
+                    return;
+                case Key.Escape:
+                    quitGame();
+                    return;
+                case Key.I:
+                    InstrucuntionManual();
+                    return;
+                default:
+                    if (!game_start) MessageBox.Show("Press Enter to start the game, Esc to quit, or I for instructions");
+                    else MessageBox.Show("Game in progress, use only the mouse to play");
+                    return;
+            }
         }
 
         private void second_Move_request(GUI pg, Point[] moves)
@@ -56,11 +138,17 @@ namespace Morabaraba_2
             }
             pg.playerTurn.Content = "Click the \nMOVE TO point";
             pg.player_err_msg.Visibility = Visibility.Hidden;
-           
+
         }
 
         private void Background_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if (!game_start)
+            {
+                MessageBox.Show("To start the game, first click the 'START A NEW GAME' button");
+                return;
+            }
+
             if (game.draw) return;
             if (game_over) return;
             if(game.mill)
@@ -68,7 +156,8 @@ namespace Morabaraba_2
                 game.messageDisplay("The is mill, first choose a \ncow to eliminate before \ncontinuing, use mouse \nleft click to kill");
                 return;
             }
-            Point p1 = Mouse.GetPosition(Background);
+
+            Point p1 = Mouse.GetPosition(background);
 
             if (moves[0].X == 0) moves[0] = p1;
             else moves[1] = p1;
@@ -91,29 +180,17 @@ namespace Morabaraba_2
             game.player1GUI.GUI_update();
             game.player2GUI.GUI_update();
         }
-       
-        private void gameOver(GUI pgWinner, GUI pgLoser)
-        {
-            pgWinner.playerTurn.Visibility = Visibility.Visible;
-            pgLoser.playerTurn.Visibility = Visibility.Visible;
-            pgWinner.player_err_msg.Visibility = Visibility.Visible;
-            pgLoser.player_err_msg.Visibility = Visibility.Visible;
-
-            pgWinner.playerTurn.Content = "WINNER";
-            pgWinner.player_err_msg.Content = "CONGRADULATION\n  you have won the game";
-            pgLoser.playerTurn.Content = "LOSER";
-            pgLoser.player_err_msg.Content = "You have lost the game\n  try harder next time!";
-
-            game_over = true;
-        }
-
-       
 
         private void Background_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if(!game_start)
+            {
+                MessageBox.Show("To start the game, first click the 'START A NEW GAME' button");
+                return;
+            }
             if (game.draw) return;
             if (game_over) return;
-            Point p = Mouse.GetPosition(Background);
+            Point p = Mouse.GetPosition(background);
             if (game.mill)
             {
                 game.Eliminate(p);
@@ -129,5 +206,9 @@ namespace Morabaraba_2
             }
             game.isMill();
         }
+
+        
+
+        
     }
 }
